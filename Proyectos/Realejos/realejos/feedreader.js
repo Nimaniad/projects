6239e3f65f98bc01b77e4db8419ad78eb79f8cad
Nimaniad <adri.glez.98@gmail.com>
2021-03-16 11:41:43 +0000

    Cambios en el script, ahora si lee la descripción

--- Proyectos/Realejos/realejos/feedreader.js
+++ Proyectos/Realejos/realejos/feedreader.js
@@ -4,10 +4,16 @@ fetch(RSS_URL)
     .then(response => response.text())
     .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
     .then(data => {
+        var parser = new DOMParser;
         console.log(data);
         const items = data.querySelectorAll("item");
         let html = ``;
         items.forEach(el => {
+
+            var description = el.querySelector('description');
+            description = parser.parseFromString(description.textContent, 'text/html');
+
+
             html += `
         <div class="carousel-item active" data-bs-interval="10000">
             <div class="container-item">
@@ -19,7 +25,7 @@ fetch(RSS_URL)
           </h5>
           
           <p class="description ms-5">
-          ${el.querySelector("description").innerHTML}
+          ${description.body.innerHTML}
          
               </p>
               </div>
@@ -35,57 +41,3 @@ fetch(RSS_URL)
     });
 let html2 = `</div></div>`
 document.body.insertAdjacentHTML("beforeend", html2);
-
-
-
-/*loadXMLFeed = () => {
-    const url = "rss.xml";
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            let parser = new DOMParser();
-            let xml = parser.parseFromString(data, "application/xml");
-            displayList(xml);
-        })
-
-}
-document.addEventListener("DOMContentLoaded", loadXMLFeed);
-
-function displayList(a) {
-    let list = document.getElementById('item');
-    let item = a.getElementsByTagName('item');
-    let itemNum = a.getElementsByTagName('item').length;
-    console.log(itemNum);
-
-
-
-
-    for (let i = 0; i > itemNum; i++) {
-        let li = document.createElement('li');
-        li.className = "listItem";
-
-        li.innerHTML =
-            `
-        <h3>${item[i].getElementsByTagName('title')[0].innerHTML}</h3>
-        <p>${item[i].getElementsByTagName('description')[0].innerHTML}</p>
-        `;
-
-        list.appendChild(li);
-    }
-}
----------------------------------------------------------------------------------------
----------------------------------------------------------------------------------------
----------------------------------------------------------------------------------------
-
-let urlRSS = 'https://script.echeide.com/losrealejos_rss/';
-
-$http.get(urlRSS).then(function(response) {
-    if (response.status == 200) {
-        var parser = new DOMParser;
-        var doc = parser.parserFromString(response.data, 'text/xml')
-
-        var item = doc.querySelectorAll('item')
-
-    }
-});
-*/
