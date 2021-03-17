31f1237b54074fd0bfa13f298686dba6fa50d36c
Nimaniad <adri.glez.98@gmail.com>
2021-03-17 14:31:07 +0000

    Inteno de arreglo con condicional

--- Proyectos/Realejos/realejos/feedreader.js
+++ Proyectos/Realejos/realejos/feedreader.js
@@ -8,36 +8,61 @@ fetch(RSS_URL)
         console.log(data);
         const items = data.querySelectorAll("item");
         let html = ``;
-        items.forEach(el => {
-
-            var description = el.querySelector('description');
-            description = parser.parseFromString(description.textContent, 'text/html');
-
-
-            html += `
-        <div class="carousel-item active" data-bs-interval="10000">
+        if (i == 0) {
+            items.forEach(el => {
+                var description = el.querySelector('description');
+                description = parser.parseFromString(description.textContent, 'text/html');
+                html += `
+            <div class="carousel-item active" data-bs-interval="10000">
             <div class="container-item">
         <article>
-          <img src="${el.querySelector("thumbnail").innerHTML}" alt="">
+          <img src="${el.querySelector("thumbnail").innerHTML}" class="image d-block w-100" alt="">
           <div class="carousel-caption position-absolute bottom-0 start-0  mb-3 d-block">
+          <div class="carousel-child">
           <h5 class="title text-uppercase ms-5 pt-4">
               ${el.querySelector("title").innerHTML}
           </h5>
-          
           <p class="description ms-5">
           ${description.body.innerHTML}
-         
               </p>
               </div>
               </div>
-          </div>
+              </article>
+         </div>
       </div>
-        </article>
+        
       `;
-        });
+
+            });
+        } else {
+            items.forEach(el => {
+                var description = el.querySelector('description');
+                description = parser.parseFromString(description.textContent, 'text/html');
+                html += `
+            <div class="carousel-item" data-bs-interval="10000">
+            <div class="container-item">
+        <article>
+          <img src="${el.querySelector("thumbnail").innerHTML}" class="image d-block w-100" alt="">
+          <div class="carousel-caption position-absolute bottom-0 start-0  mb-3 d-block">
+          <div class="carousel-child">
+          <h5 class="title text-uppercase ms-5 pt-4">
+              ${el.querySelector("title").innerHTML}
+          </h5>
+          <p class="description ms-5">
+          ${description.body.innerHTML}
+              </p>
+              </div>
+              </div>
+              </article>
+         </div>
+      </div>
+        
+      `;
+
+            });
+
+        }
         document.body.insertAdjacentHTML("beforeend", html);
 
 
     });
-let html2 = `</div></div>`
-document.body.insertAdjacentHTML("beforeend", html2);
