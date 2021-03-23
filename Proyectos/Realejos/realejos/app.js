e5839fd1bf53bc4bb8645fcc8a452e821f8c864a
Nimaniad <adri.glez.98@gmail.com>
2021-03-23 15:06:05 +0000

    Limpieza del código

--- Proyectos/Realejos/realejos/app.js
+++ Proyectos/Realejos/realejos/app.js
@@ -11,7 +11,7 @@ fetch(RSS_URL)
         var parser = new DOMParser;
         var id = 0;
 
-        console.log('data', data);
+
 
 
         const items = [...data.querySelectorAll("item")].map(item => {
@@ -22,85 +22,10 @@ fetch(RSS_URL)
             }
         })
         let html = ``;
-
+        console.log('data', data);
 
         const carousel = new Carousel(document.body, items);
 
         carousel.play();
 
-
-
-        // console.log(items);
-
-        // var timer = setInterval(function() {
-        //     for (var i = 0; i < items.length; i++) {
-        //         console.log(items[i]);
-        //         if (i == 0 && !items[i].play) {
-        //             var description = items[i].querySelector('description');
-
-        //             description = parser.parseFromString(description.textContent, 'text/html');
-        //             html += `<div class="carousel-item active" data-bs-interval="10000">
-        //                     <div class="container-item">
-        //                         <article>
-        //                             <img src="${items[i].querySelector("thumbnail").textContent}" class="image d-block w-100" alt="">
-        //                                 <div class="carousel-caption position-absolute bottom-0 start-0  mb-3 d-block">
-        //                                     <div class="carousel-child">
-        //                                         <h5 class="title text-uppercase ms-5 pt-4">
-        //                                             ${items[i].querySelector("title").innerHTML}
-        //                                         </h5>
-        //                                         <p class="description ms-5">
-        //                                             ${description.body.innerHTML}
-        //                                         </p>
-        //                                     </div>
-        //                                 </div>
-        //                         </article>
-        //                     </div>
-        //                 </div>
-        //                 `;
-
-        //             document.body.insertAdjacentHTML("beforeend", html);
-        //             console.log(html);
-        //             html = ``;
-
-        //             items[i].play = true
-        //             break;
-        //         }
-        //         if (i > 0 && !items[i].play) {
-
-
-        //             var description = items[i].querySelector('description');
-
-        //             description = parser.parseFromString(description.textContent, 'text/html');
-        //             html += `<div class="carousel-item" data-bs-interval="10000">
-        //                     <div class="container-item">
-        //                         <article>
-        //                             <img src="${items[i].querySelector("thumbnail").textContent}" class="image d-block w-100" alt="">
-        //                                 <div class="carousel-caption position-absolute bottom-0 start-0  mb-3 d-block">
-        //                                     <div class="carousel-child">
-        //                                         <h5 class="title text-uppercase ms-5 pt-4">
-        //                                             ${items[i].querySelector("title").innerHTML}
-        //                                         </h5>
-        //                                         <p class="description ms-5">
-        //                                             ${description.body.innerHTML}
-        //                                         </p>
-        //                                     </div>
-        //                                 </div>
-        //                         </article>
-        //                     </div>
-        //                 </div>
-        //  `;
-
-        //             document.body.insertAdjacentHTML("beforeend", html);
-        //             console.log(html);
-        //             html = ``;
-
-
-        //             items[i].play = true
-        //             break;
-        //         }
-        //     }
-        // }, 10000);
-
-        // html += `</div></div>`;
-        // document.body.insertAdjacentHTML("beforeend", html);
     });
